57bb11efdf7b544ab5be9576987ef3916908992d
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-01-27 16:16:52 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <h1>Salam React.js</h1>
+      <p>some description</p>
+      <Footer />
+    </>
   );
 }
 
 export default App;
+// default -> export default App;   -> import App from './filePath'
+// named -> export const name='ali'     --> import {name as test} from "./filePath"
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg"
+
+function Header() {
+    return <header>
+        <h1>salam</h1>
+        <img
+            style={{
+                width: 100,
+                height: 100,
+            }}
+            src={logo} alt="mern02 react logo" />
+    </header>
+}
+export default Header;
+
+// elem.style.width = '100px'
+// jsx -> 
